122fa135c6f59abf43699cdd004b54c8a47393af
manu2raj <manu2raj@yahoo.co.in>
2025-10-27 16:20:22 +0530

    20. Configuring cors on API Added cors in Program.cs file in API project

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,13 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
+app.UseCors(x=> x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("http://localhost:3000","https://localhost:3000"));
 //// Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{

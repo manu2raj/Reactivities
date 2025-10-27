02d173b21ddde2058b49e85999543e016edc2e20
manu2raj <manu2raj@yahoo.co.in>
2025-10-27 17:02:15 +0530

    21. Creating an Activities typs in typescript Added file index.d.ts File contains type of response we getting via api. i.e. Activity.
    I use json to typescript converter to get type and members
    
    Also updated same Activity type in useState. So if there is any typo, while using Activity type member, it will show error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch('https://localhost:5001/api/activities')

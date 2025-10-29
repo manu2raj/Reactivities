19268ae52949117fbdd67e5daa93e610843082fe
manu2raj <manu2raj@yahoo.co.in>
2025-10-29 17:56:52 +0530

    45. Creating a cards for the activities - part1 a. Created new directory -> dashboard inside activities b. Created component ActivityCard.tsx. This will show information of activity in Card c. Created component ActivityList.tsx, this will be responsible to iterating all activity in activities list and return ActivityCard for all activity d. Move Activity iteation code from ActivityDashboard to ActivityList

--- client/src/features/activities/ActivityDashborad.tsx
+++ client/src/features/activities/ActivityDashborad.tsx
@@ -1,4 +1,5 @@
-import { Grid2, List, ListItem, ListItemText } from "@mui/material";
+import { Grid2 } from "@mui/material";
+import ActivityList from "./dashborad/ActivityList";
 
 type Props = {
     activities: Activity[];
@@ -8,14 +9,7 @@ export default function ActivityDashborad({activities}: Props) {
     return (
         <Grid2 container>
             <Grid2 size={9}>
-                <List>
-                    {
-                        activities.map(activity => (
-                            <ListItem key={activity.id}>
-                                <ListItemText>{activity.title}</ListItemText>
-                            </ListItem>
-                        ))}
-                </List>
+                <ActivityList activities={activities}/>
             </Grid2>
         </Grid2>
     )

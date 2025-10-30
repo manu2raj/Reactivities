0898f38a7597356a9b2236ce19ba4725e7f2ca2c
manu2raj <manu2raj@yahoo.co.in>
2025-10-30 15:50:58 +0530

    48. Creating an Activity from component a. Created directory form b. Created component ActivityForm.tsx c. Modify ActivtyDashboard to show ActivityForm form below ActivityDetail

--- client/src/features/activities/dashborad/ActivityDashborad.tsx
+++ client/src/features/activities/dashborad/ActivityDashborad.tsx
@@ -1,6 +1,7 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
 import ActivityDetail from "../details/ActivityDetail";
+import ActivityForm from "../form/ActivityForm";
 
 type Props = {
     activities: Activity[]
@@ -19,11 +20,13 @@ export default function ActivityDashborad({activities, selectActivity, cancelSel
                 />
             </Grid2>
             <Grid2 size={5}>
-                {selectedActivity && <ActivityDetail 
-                    activity={selectedActivity} 
-                    cancelSelectActivity={cancelSelectActivity}
-                />
+                {selectedActivity &&   /*If selectedActivity is not undefined then show ActivityDetail*/
+                    <ActivityDetail 
+                        activity={selectedActivity} 
+                        cancelSelectActivity={cancelSelectActivity}
+                    />
                 }
+                <ActivityForm />
             </Grid2>
         </Grid2>
     )
---
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,24 @@
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+
+
+export default function ActivityForm() {
+  return (
+    <Paper sx={{borderRadius: 3, p: 3}}>
+        <Typography variant="h5" color="primary">
+            Create activities
+        </Typography>
+        <Box component='form' display='flex' flexDirection='column' gap={3}>
+            <TextField label='Title' />
+            <TextField label='Description' multiline rows={3} />
+            <TextField label='Category' />
+            <TextField label='Date' type="date" />
+            <TextField label='City' />
+            <TextField label='Venue' />
+            <Box display={"flex"} justifyContent='end' gap={3}>
+                <Button color='inherit' >Cancel</Button>
+                <Button color='success' variant="contained">Submit</Button>
+            </Box>
+        </Box>
+    </Paper>
+  )
+}

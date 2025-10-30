65fdba982783f1ffaf5c489598080f46dbc4f529
manu2raj <manu2raj@yahoo.co.in>
2025-10-30 14:59:06 +0530

    47. Selecting an individual activity to view This is to show activity detail, when user click on View button of individual activity
    Changes:
    a.  App.tsx: since we are managing state of activities in current component, so we will set selectedActivity from current class.
       i. here we will create const for selectedActivity and function to handleSelectActivity and handleCancelSelectedActivity
       ii. All this information will be passed to ActivityDashborad. As inside ActivityDashboard, we are iterating Activities
    
    b. ActivityDashborad: Here we accept selectActivity and cancelSelectActity and selectedActivity as props. And pass them to ActivityList and ActivityDetail
    
    c. From ActivityList component, selectActivity function will pass to ActivityCard component
    
    d. Inside ActivityCard component selectActivity function will be used by view button onClick event
    
    e. Similarly, inside ActivityDetail, cancelSelectActivity function will be used by cancel bution onClick event

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,18 +6,31 @@ import ActivityDashborad from "../../features/activities/dashborad/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get<Activity[]>('https://localhost:5001/api/activities')
       .then(response => setActivities(response.data));
   }, []);
 
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(a => a.id === id));
+  }
+
+  const handleCancelSelectActivity = () => {
+    setSelectedActivity(undefined);
+  }
+
   return (
     <Box sx={{bgcolor: '#eeeeee'}}>
       <CssBaseline />
       <NavBar />
       <Container maxWidth="xl" sx={{mt: 3}}>
-        <ActivityDashborad activities={activities}/>
+        <ActivityDashborad 
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectedActivity={selectedActivity} />
       </Container>      
     </Box>
   )

4e2babae0db4607405e9df30a402f5bbdd84c894
manu2raj <manu2raj@yahoo.co.in>
2025-11-05 19:21:15 +0530

    61. Creating an activty with a React query mutation Based on activity exist or not, update and create method will get called
    Changes:
    1. useActivities.ts: create new function createActivity with agent.post
    
    2. ActivityForm.tsx: from useActivities() hook, get createActivty method. And used it in handleSubmit(), if activity not exist
    
    Also added comments

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -2,30 +2,40 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../agent";
 
 export const useActivities = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient();   // Get the QueryClient instance
 
 // Placeholder for activity state and logic
-  const {data: activities, isPending} = useQuery({
-    queryKey: ['activities'],
-    queryFn: async () => { 
-      const response = await agent.get<Activity[]>('/activities');
-      return response.data;
+  const {data: activities, isPending} = useQuery({    // Fetch activities using React Query
+    queryKey: ['activities'],                         // Unique key for the query
+    queryFn: async () => {                            // Function to fetch activities
+      const response = await agent.get<Activity[]>('/activities');  // Send GET request to fetch activities
+      return response.data;                         // Return the data from the response
     }  
   })
   
-  const updateActivity = useMutation ({
+  const updateActivity = useMutation ({   // Mutation for updating an activity
     mutationFn: async (activity: Activity) => {
-      await agent.put('/activities', activity);
+      await agent.put('/activities', activity);   // Send PUT request to update activity
     },
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({ queryKey: ['activities'] });
+    onSuccess: async () => {    // Invalidate activities query to refetch data
+      await queryClient.invalidateQueries({ queryKey: ['activities'] });    // Invalidate activities query to refetch data
     }
   });
 
-  return { 
+  const createActivity = useMutation ({   // Mutation for creating a new activity
+    mutationFn: async (activity: Activity) => {
+      await agent.post('/activities', activity);  // Send POST request to create activity
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ['activities'] });  //  Invalidate activities query to refetch data
+    }
+  });
+
+  return {    // Return activities and mutation functions
      activities,
      isPending,
-     updateActivity
+     updateActivity,
+     createActivity
     };
 }
 

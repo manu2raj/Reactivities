634d425ed6a5558f9a50be57b3d37a429697befd
manu2raj <manu2raj@yahoo.co.in>
2025-11-10 16:11:35 +0530

    77. Adding filters component Added react-calendar package. as we will use it in filter page
    Changes:
    1.  Styles.css: added style for .react-calendar
    Note using !important: The !important flag in CSS is used to give a specific property value the highest priority, overriding all other styling rules for that property on a given element, regardless of specificity or source order.
    
    2. ActivityFilter.tsx: Created new filter component. based on events, on going and self  hosting. Also based on date
    
    3. ActivityDashborad.tsx: Added newly created filter component

--- client/src/features/activities/dashborad/ActivityDashborad.tsx
+++ client/src/features/activities/dashborad/ActivityDashborad.tsx
@@ -1,14 +1,15 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilter from "./ActivityFilter";
 
 export default function ActivityDashborad() {
     return (
-        <Grid2 container spacing={3}>
+        <Grid2 container spacing={8}>
             <Grid2 size={7}>
                 <ActivityList />
             </Grid2>
-            <Grid2 size={5}>
-                Activty filter go here
+            <Grid2 size={4}>
+                <ActivityFilter />
             </Grid2>
         </Grid2>
     )

a2f8c33c0eac497c862b5b78ac2e9b8c152f6389
manu2raj <manu2raj@yahoo.co.in>
2025-11-10 21:34:07 +0530

    79. Formatting the dates date-fns: date-fns provides the most comprehensive, yet simple and consistent toolset for manipulating JavaScript dates in a browser & Node.js.
    Changes:
    1. Package.json: Added lib: date-fns
    2. util.ts: created class for common function
    3. ActivityCard.tsx: Added formatDate funtion, that we have created in util.ts file
    4. ActivityDetialsHeader.tsx: Added formatDate funtion, that we have created in util.ts file
    5. ActivityDetailsInfo.tsx: Added formatDate funtion, that we have created in util.ts file

--- client/src/features/activities/dashborad/ActivityCard.tsx
+++ client/src/features/activities/dashborad/ActivityCard.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Button, Card, CardContent, CardHeader, Chip, Divider, Typography } from "@mui/material";
 import { Link } from "react-router";
 import { AccessTime, Place } from "@mui/icons-material";
+import { formatDate } from "../../../lib/util/util";
 
 type Props = {
     activity: Activity;
@@ -43,8 +44,12 @@ export default function ActivityCard({activity}: Props) {
 
       <CardContent sx={{p:0}}>                                {/* Card content with activity details like date and venue and attendies*/}
         <Box display='flex' alignItems='center' mb={2} px={2}>
-          <AccessTime sx={{mr: 1}}/>
-          <Typography variant="body2">{(activity.date)}</Typography>
+          <Box display={'flex'} flexGrow={0} alignItems={'center'}>
+            <AccessTime sx={{mr: 1}}/>
+            <Typography variant="body2" noWrap>
+              {formatDate(activity.date)}
+            </Typography>
+          </Box>
           <Place sx={{ml:3 , mr:1}} />
           <Typography variant="body2">{activity.venue}, {activity.city}</Typography>
           
---
+++ client/src/lib/util/util.ts
@@ -0,0 +1,6 @@
+import type { DateArg } from "date-fns";
+import  { format } from "date-fns";
+
+export function formatDate(date: DateArg<Date>) {
+    return format(date, 'dd MMM yyyy h:mm a');
+}

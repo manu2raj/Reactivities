f0bcbebf676eb6bacf0a423ac3c1ac54b1c3ea2c
manu2raj <manu2raj@yahoo.co.in>
2025-11-11 20:18:29 +0530

    88. Adding loading state
    Changes:
    1. uiStore.ts: create class UiStore with property isLoading, and method isBusy and isIdle
    2. store.ts: create uiStore property in Store interface and initialize that it in Store
    3. agent.ts: add agent request use, and set uiStore busy and in response finally, set uiStore to isIdle
    4. NavBar.tsx: Add observer, this will set LinearProgress is isLoading is true. In case of setting request

--- client/src/lib/agent.ts
+++ client/src/lib/agent.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "./stores/store";
 
 const sleep = (delay: number) => {  // Function to simulate network delay
     return new Promise((resolve) => {   // Return a promise that resolves after the specified delay
@@ -9,6 +10,11 @@ const agent = axios.create({              // Create an Axios instance
   baseURL: import.meta.env.VITE_API_URL   // Set base URL from environment variable
 });
 
+agent.interceptors.request.use(config => {  
+    store.uiStore.isBusy();
+    return config;
+})
+
 agent.interceptors.response.use(async response => {
     try {
         await sleep(1000);      // Simulate network delay of 1 second
@@ -16,6 +22,8 @@ agent.interceptors.response.use(async response => {
     } catch (error) {
         console.log(error);
         return Promise.reject(error);   // Reject the promise in case of error
+    } finally {
+        store.uiStore.isIdle();
     }
 });
 

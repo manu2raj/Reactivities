64230d31c51fa08a9c68903e30bbf9a383821f67
manu2raj <manu2raj@yahoo.co.in>
2025-11-12 19:11:01 +0530

    92. Validation with Data annotation while creating new activity, use annotation for showing proper validation
    Changes:
    1. CreateActivityDto.cs: create new DTO file, copy fields from Activity.cs file.
           Remove required keyword and add Required attribute
           For string property, set string.empty as default value
    2. MappingProfiles.cs: Create mapping for CreateActivityDto to Activity
    3. CreateActivity.cs: In command class, use CreateActivityDto and in handler convert dto to activity
    4. ActivitiesController.cs: use CreateActivityDto in place of activity

--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Domain;
 using Persistence;
+using Application.Activities.DTOs;
+using AutoMapper;
 
 namespace Application.Activities.Commands
 {
@@ -8,16 +10,17 @@ namespace Application.Activities.Commands
     {
         public class Command : IRequest<string>
         {
-            public required Activity Activity { get; set; }
+            public required CreateActivityDto ActivityDto { get; set; }
         }
 
-        public class Handler(AppDbContext context) : IRequestHandler<Command, string>
+        public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, string>
         {
             public async Task<string> Handle(Command request, CancellationToken cancellationToken)
             {
-                context.Activities.Add(request.Activity);
+                var activity = mapper.Map<Activity>(request);
+                context.Activities.Add(activity);
                 await context.SaveChangesAsync(cancellationToken);
-                return request.Activity.Id;
+                return activity.Id;
             }
         }
     }
--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Application.Activities.DTOs;
+using AutoMapper;
 using Domain;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ namespace Application.Core
         {
             // CreateMap<Source, Destination>();
             CreateMap<Activity, Activity>();
+            CreateMap<CreateActivityDto, Activity>();
         }
     }
 }

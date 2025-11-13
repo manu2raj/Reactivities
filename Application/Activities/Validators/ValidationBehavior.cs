1c8dc42821ef85d256306517e9114fcf2820367a
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-13 14:41:13 +0530

    94. Adding validation middleware
    Changes
    1. Command\CreateActivity.cs:
       Remove validator from CreateActivity handler as we relay on mediator, to do this for us.
    
    2. ValidationBehavior.cs: Create middleware to validate input request
    
    3. Program.cs: Add ValidationBehavior in AddMediaR.

---
+++ Application/Activities/Validators/ValidationBehavior.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Activities.Validators
+{
+    /// <summary>
+    /// Generic middleware, to validate input request.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="validator"></param>
+    public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null)
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            // If no validator is provided, continue to the next handler in the pipeline.
+            if (validator == null) return await next(); 
+
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
+            return await next();
+        }
+    }
+}

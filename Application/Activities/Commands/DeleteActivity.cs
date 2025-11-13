b46f2d59a40c896d7a9d44952035cdd1abcd7424
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-13 12:43:15 +0530

    Retest commit in main branch

--- Application/Activities/Commands/DeleteActivity.cs
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -1,5 +1,10 @@
 ﻿using MediatR;
 using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Application.Activities.Commands
 {

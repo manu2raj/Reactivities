20414c7e8abd69bcba3e77297962c55ae9ec6523
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-13 12:39:52 +0530

    Test commit

--- Application/Activities/Commands/DeleteActivity.cs
+++ Application/Activities/Commands/DeleteActivity.cs
@@ -1,10 +1,5 @@
 ﻿using MediatR;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Activities.Commands
 {

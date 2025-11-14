73c4dce2e9aa0adf42828e443996d70177cf54a6
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-14 15:40:53 +0530

    102. Setting up the client for the error handling
    Changes:
    1. BuggyController.cs: Add new controller to bug api. to implement same at client side
    2. TestErrors.tsx: Created to demonstate, to handle error at clien siide.
    3. Route.tsx: Add route for errors.tsx
    4. NavBar.tsx: Add menu for errors.tsx
    5. Package.json: add package react-toastify
    6. main.tsx: add css and  ToastifyContainer

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -39,6 +39,10 @@ export default function NavBar() {
                     <MenuItemLink to='/counter'>
                       Counter
                     </MenuItemLink>
+
+                    <MenuItemLink to='/errors'>
+                      Error
+                    </MenuItemLink>
                 </Box>
                 <MenuItem>
                 User Menu

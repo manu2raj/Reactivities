cce64cff6a2dfd87d9790450a83963f221c3fd70
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-14 16:04:04 +0530

    103. Using an Axios interceptor to handle errors
    Changes:
    agent.ts: update agent response code
      handle error with status code

--- client/src/lib/agent.ts
+++ client/src/lib/agent.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { store } from "./stores/store";
+import { toast } from "react-toastify";
 
 const sleep = (delay: number) => {  // Function to simulate network delay
     return new Promise((resolve) => {   // Return a promise that resolves after the specified delay
@@ -15,16 +16,38 @@ agent.interceptors.request.use(config => {
     return config;
 })
 
-agent.interceptors.response.use(async response => {
-    try {
+agent.interceptors.response.use(
+    async response => {
         await sleep(1000);      // Simulate network delay of 1 second
-        return response;        // Return the response after delay
-    } catch (error) {
-        console.log(error);
-        return Promise.reject(error);   // Reject the promise in case of error
-    } finally {
         store.uiStore.isIdle();
+        return response;        // Return the response after delay
+    },
+    async error => {
+        await sleep(1000);      // Simulate network delay of 1 second
+        store.uiStore.isIdle();
+
+        //console.log('axios error: ' + error);        // Return the response after delay
+        const {status} = error.response;
+        switch (status)
+        {
+            case 400:
+                toast.error('bad request')
+                break;
+            case 401:
+                toast.error('Unauthorized')
+                break;
+            case 404:
+                toast.error('Not found')
+                break;
+            case 500:
+                toast.error('Server Error')
+                break;
+            default:
+                break;
+        }
+
+        return Promise.reject(error);                 // Reject the promise in case of error
     }
-});
+);
 
 export default agent;       // Export the Axios instance for use in other parts of the application

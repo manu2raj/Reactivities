2f292c1fcabacce6a33463c910edd26559a4ece8
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-17 17:54:38 +0530

    113. Creating a reusable Select Input
    Changes
    1. SelectInputs.tsx: Create custom component control. i.e. Combobox
    2. TextInput.tsx: Fix error, changing a controlled input to be uncontrolled
    3. categoryOptions.ts: List of input array string for SelectInput, that we used for activity
    4. ActivityForm: Use selectInput in place of TextInput

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import TextInput from "../../../app/shared/components/TextInput";
+import SelectInput from "../../../app/shared/components/SelectInput";
+import { categoryOptions } from "./categoryOptions";
 
 export default function ActivityForm() {
     const { control, reset, handleSubmit } = useForm<ActivitySchema>({
@@ -33,7 +35,10 @@ export default function ActivityForm() {
         <Box component='form' onSubmit={handleSubmit(onSubmit)} display='flex' flexDirection='column' gap={3}>
             <TextInput label='Title' control={control} name='title' />
             <TextInput label='description' control={control} name='description' multiline rows={3} />
-            <TextInput label='category' control={control} name='category' />
+            <SelectInput items={categoryOptions}
+             label='category' 
+             control={control}
+             name='category' />
             <TextInput label='date' control={control} name='date' />
             <TextInput label='city' control={control} name='city' />
             <TextInput label='venue' control={control} name='venue' />

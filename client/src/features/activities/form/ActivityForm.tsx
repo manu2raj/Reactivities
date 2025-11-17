3a9308824ec80f8bf5b95844cd76850e74099d19
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-17 17:33:09 +0530

    112. Creating a reusable Text Input
    Changes.
    1. TextInput.tsx: create custom share component for text input. `useController` with the provided controller props. It is created via TextInput
    2. ActivityForm.tsx: Use custom shared component. i.e. TextInput,

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,14 @@
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useParams } from "react-router";
 import { useForm } from "react-hook-form"
 import { useEffect } from "react";
 import { activitySchema, type ActivitySchema } from "../../../lib/schemas/activitySchema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import TextInput from "../../../app/shared/components/TextInput";
 
 export default function ActivityForm() {
-    const { register, reset, handleSubmit, formState: {errors} } = useForm<ActivitySchema>({
+    const { control, reset, handleSubmit } = useForm<ActivitySchema>({
         mode: "onTouched",
         resolver: zodResolver(activitySchema)
     })
@@ -30,22 +31,12 @@ export default function ActivityForm() {
             {activity ? 'Edit Activity' : 'Create Activities'}
         </Typography>
         <Box component='form' onSubmit={handleSubmit(onSubmit)} display='flex' flexDirection='column' gap={3}>
-            <TextField 
-                {...register('title')}
-                label='Title' 
-                defaultValue={activity?.title}
-                error={!!errors.title}
-                helperText={errors.title?.message}
-            />
-            <TextField {...register('description')}  label='Description' multiline rows={3}  defaultValue={activity?.description} />
-            <TextField {...register('category')}  label='Category'  defaultValue={activity?.category}  />
-            <TextField {...register('date')}  label='Date' type="date" 
-                defaultValue={activity?.date
-                    ? new Date(activity.date).toISOString().split('T')[0]
-                    :new Date().toISOString().split('T')[0]
-                }/>
-            <TextField {...register('city')}  label='City'  defaultValue={activity?.city} />
-            <TextField {...register('venue')}  label='Venue'  defaultValue={activity?.venue} />
+            <TextInput label='Title' control={control} name='title' />
+            <TextInput label='description' control={control} name='description' multiline rows={3} />
+            <TextInput label='category' control={control} name='category' />
+            <TextInput label='date' control={control} name='date' />
+            <TextInput label='city' control={control} name='city' />
+            <TextInput label='venue' control={control} name='venue' />
             <Box display={"flex"} justifyContent='end' gap={3}>
                 <Button color='inherit' >Cancel</Button>
                 <Button 

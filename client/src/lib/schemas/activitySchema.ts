b786c804cd711e51932d9c2333ddd1722660afa3
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-17 16:47:27 +0530

    111. Validating the inputs part 2
    Changes:
    1. activitySchema.ts: Added validation property for all fields. also create common method show generate error message for mandatory (required)  field
    2. Consume them in ActviityForm. also set onTouched mode, so that it will show error on ui element once control focus lost

--- client/src/lib/schemas/activitySchema.ts
+++ client/src/lib/schemas/activitySchema.ts
@@ -1,8 +1,15 @@
 import { z } from 'zod';
 
+const requiredString = (fieldName: string) => z.string().min(1, {message: `${fieldName} is required`})
+
 export const activitySchema = z.object({
-    title: z.string({required_error: 'Title is required'}).min(1, {message: 'Title is required'})
+    title: requiredString('Title'),
+    description: requiredString('Description'),
+    category: requiredString('Category'),
+    date: requiredString('Date'),
+    city: requiredString('City'),
+    venue: requiredString('Venue'),
     // title: z.string().min(10, { message: 'Title is required' })
 })
 
-export type activitySchema = z.infer<typeof activitySchema>;
+export type ActivitySchema = z.infer<typeof activitySchema>;

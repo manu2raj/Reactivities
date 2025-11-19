cc39457af83eea831c529083b66701a37ca96fb8
Manuraj <86165841+manu2raj@users.noreply.github.com>
2025-11-19 22:01:58 +0530

    127. Registering new users
    Changes:
    1 RegisterDto: Create dto class in API project, as we are not using fluent for validation. that we are performing in Application project
    2. AccountController.cs: Create api for registering user

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using API.DTOs;
+using Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(SignInManager<User> signInManager) : BaseApiController
+    {
+        [AllowAnonymous]
+        [HttpPost ("register")]
+        public async Task<ActionResult> RegisterUser(RegisterDto registerDto)
+        {
+            var user = new User()
+            {
+                UserName = registerDto.Email,
+                Email = registerDto.Email,
+                DisplayName = registerDto.DisplayName,
+            };
+
+            var result = await signInManager.UserManager.CreateAsync(user, registerDto.Password);
+
+            if (result.Succeeded) return Ok();
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return ValidationProblem();
+        }
+    }
+}
